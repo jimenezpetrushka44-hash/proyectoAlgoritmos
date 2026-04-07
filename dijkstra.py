25c8e53a3b44810de4a71050a7abf0c11f9ba8b6
Anna Jimenez <jimenezpetrushka44@gmail.com>
2026-04-07 15:35:34 -0600

    Cree el documento y los primeros pasos para darle vida al algortimo

---
+++ dijkstra.py
@@ -0,0 +1,11 @@
+#Este es el codigo de dikjstra
+
+#Creando la clase:
+class Dijkstra():
+    def __init__(self, maze):
+        self.maze = maze
+        self.n = len(maze)
+        
+    def run(self, start, end):
+        
+    

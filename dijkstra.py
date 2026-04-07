d79bb69befd20794f5c1cebb5d093112a1a00a5c
Anna Jimenez <jimenezpetrushka44@gmail.com>
2026-04-07 16:46:17 -0600

    Testing the algo

--- dijkstra.py
+++ dijkstra.py
@@ -7,7 +7,8 @@ class Dijkstra():
     def __init__(self, maze):
         self.maze = maze
         self.n = len(maze)
-        
+    
+    #Metodo q guaarda al algortimo en si
     def run(self, start, end):
         dist = { }
         dist[start] = 0 
@@ -19,12 +20,60 @@ class Dijkstra():
             current_dist, (x,y) = heapq.heappop(priority_queue) #Saca el nodo menor, mas bajo
             if (x,y) == end:
                 break
+            
+            #Aqui le doy los pesos, de donde va y para dnd llega
             directions = [(-1,0), (1,0), (0,-1), (0,1)]
             #Aqui recorro todas las direcciones y calculo nuevo nodo:
             for dx, dy in directions:
                 nx = x + dx
                 ny = y + dy
                 
+                #Valido si la ruta es mas corta o larga, actualizo los pesos si se debe
                 if 0 <= nx < self.n and 0 <= ny < self.n and self.maze[nx][ny] == 0:
+                    new_dist = current_dist + 1
                     
+                    #Aqui se actualiza:
+                    if (nx, ny) not in dist or new_dist < dist[(nx, ny)]:
+                        dist[(nx, ny)] = new_dist
+                        prev[(nx,ny)] = (x,y)
+                        heapq.heappush(priority_queue, (new_dist, (nx,ny)))
+                    
+        path = [ ]
+        node = end
+        if end not in prev:
+            return [ ]
+        while node in prev:
+            path.append(node)
+            node = prev[node]
+        path.append(start)
+        path.reverse()
+        return path
+    
+def main():
+    maze = [
+    [1,1,1,1,1,1,1],
+    [1,0,0,0,1,0,1],
+    [1,0,1,0,1,0,1],
+    [1,0,1,0,0,0,1],
+    [1,0,1,1,1,0,1],
+    [1,0,0,0,0,0,1],
+    [1,1,1,1,1,1,1]
+]
+
+    user_input = input("Would you like to see the algorithm?: ")
+        
+    if user_input.capitalize() == "Yes":
+        
+        obj_maze = Dijkstra(maze)
+        start = (1,1)
+        end= (len(maze)-2, len(maze)-2)
+        
+        path = obj_maze.run(start,end)
+        
+        print(f"La ruta mas eficiente segun el algoritmo de Dijkstra es: {path}")
+        
+        
+        
+if __name__ == "__main__":
+    main()
         

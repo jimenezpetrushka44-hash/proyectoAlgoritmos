ef15c6782b9c5565ecfb727b558736f554dc199e
Anna Jimenez <jimenezpetrushka44@gmail.com>
2026-04-07 16:22:39 -0600

    Codificando la valoracion y el algoritmo en si

--- dijkstra.py
+++ dijkstra.py
@@ -19,4 +19,12 @@ class Dijkstra():
             current_dist, (x,y) = heapq.heappop(priority_queue) #Saca el nodo menor, mas bajo
             if (x,y) == end:
                 break
+            directions = [(-1,0), (1,0), (0,-1), (0,1)]
+            #Aqui recorro todas las direcciones y calculo nuevo nodo:
+            for dx, dy in directions:
+                nx = x + dx
+                ny = y + dy
+                
+                if 0 <= nx < self.n and 0 <= ny < self.n and self.maze[nx][ny] == 0:
+                    
         
